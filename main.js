787c3db2c623293eae110f4a0b1969ab393fc59a
LuanBraga <luanbraga.lb@gmail.com>
2022-09-27 12:38:58 -0300

    init project

---
+++ main.js
@@ -0,0 +1,10 @@
+class Account {
+    constructor(agency, accoount, dac) {
+        this.agency = agency;
+        this.account = accoount;
+        this.dac = dac;
+        this.balance = 0;
+    }
+}
+
+module.exports = Account;
---
+++ spec/support/main.spec.js
@@ -0,0 +1,18 @@
+const Account = require('/home/luanbraga/Learn/basic-node-test/main.js');
+
+describe('Testing Account Object', () => {
+    let account;
+
+    beforeEach(() => {
+        account = new Account('54321', '66666', '0');
+    });
+    
+    it('Should init with correct parameters', () => {
+        expect(account.agency).toEqual('54321');
+    })
+
+    it('Should initiate balance value equals 0', () => {
+        expect(account.balance).toEqual(0);
+    });
+});
+
